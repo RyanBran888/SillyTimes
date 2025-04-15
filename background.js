6d68ef2fc49fa42b387cb2c874e690f6b5e927ad
Ryanbran888 <ryanbrandtstem@gmail.com>
2025-04-15 17:51:10 -0400

    meow?

--- background.js
+++ background.js
@@ -6,6 +6,7 @@ const sillyWords = [
     `beep boop`,
     `spaghetti`,
     `orange`,
+    `QUACK!`,
     `pronouns (scared emoji)`,
     `Activate Deception of the Sinful Spoils. Activate Deception tributing the monster in your hand to add from deck Hallowed Azamina. Activate Hallowed Azamina revealing Mu Rcielago from Extra Deck and sending Deception to the GY to summon Mu Rcielago. Mu Rcielago searches from deck Sinful Spoils WANTED from deck. Activate WANTED to search for Diabellstar from deck. Use Diabellstar's summon effect to summon herself by sending any card from hand. Activate Diabellstar's on-summon effect to set from deck "Original Sinful Spoils - Snake-Eye". Link summon "Moon of the Closed Sky" using Diabellstar + Mu Rcielago... (etc, etc, your full summon ritual continues here)`
 ];
@@ -15,23 +16,24 @@ function forcedSilly()
 {
     const width = 500;
     const height = 500;
-    const left = Math.floor(Math.random()*(screen.width - width));
-    const top = Math.floor(Math.random() * (screen.height - height));
+   // if(typeof window !== 'undefined')
+   // {
+    const left = Math.floor(Math.random()*(screen.availWidth - width));
+    const top = Math.floor(Math.random() * (screen.availHeight - height));
     const sillyWord = Math.floor(Math.random() * sillyWords.length);
-
+    const sillyWordContent = sillyWords[sillyWord];
     chrome.windows.create({
-        url: `data:text/html,
-        <html>
-        <body style='background:pink;font-family:Comic Sans MS;text-align:center;padding:1em;'>
-        <h1>${sillyWords[sillyWord]}</h1>
-        </body>
-        </html>`,
+        url: `data:text/html,<html><body style="background:pink;font-family:Comic Sans MS;text-align:center;padding:1em;"><h1>${sillyWordContent}</h1></body></html>`,
         type: "popup",
         width: width,
         height: height,
         left: left,
         top: top
     });
+//}else 
+//{
+    //console.error("mneow :(");
+//}
 }
 
 function starting() {
